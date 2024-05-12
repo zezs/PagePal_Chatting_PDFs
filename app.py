cc43991b7cba8605e4c9d7f379e692831e999b6f
zezs <my.version.ctrl@gmail.com>
2024-05-12 10:04:32 +0200

    Extract text from PDFs feature added

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 def main():
     load_dotenv()
@@ -10,8 +20,17 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click on 'Process'")
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+
+                # get the text chunks
+
+                #create vector store
         
 
 if __name__ =='__main__':
