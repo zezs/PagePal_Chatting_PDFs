21a5f2b5e9c7d4429fbb81349b924eae1fde0f4e
zezs <my.version.ctrl@gmail.com>
2024-05-12 09:51:42 +0200

    setting up stearlit frontend

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import streamlit as st
-
-
+from dotenv import load_dotenv
 
 def main():
-    st.set_page_config(page_title="PagePal:Chat with multiple PDFs", page_icon=":books:")
+    load_dotenv()
+    st.set_page_config(page_title="PagePal: Chat with multiple PDFs", page_icon=":books:")
 
-    st.header("PagePal:Chat with multiple PDFs :books:")
+    st.header("PagePal: Chat with multiple PDFs :books:")
     st.text_input("Ask a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upload your PDFs here and click on 'Process'")
+        st.button("Process")
+        
 
 if __name__ =='__main__':
     main()
